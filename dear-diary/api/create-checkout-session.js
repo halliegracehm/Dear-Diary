fe8a1f62d6a9349e4d2255e132469121653deb5e
halliegracehm <halliegracehm@gmail.com>
2026-03-18 20:42:40 -0500

    Update create-checkout-session.js

--- dear-diary/api/create-checkout-session.js
+++ dear-diary/api/create-checkout-session.js
@@ -18,8 +18,8 @@ export default async function handler(req, res) {
         "customer_email": email,
         "line_items[0][price]": priceId,
         "line_items[0][quantity]": "1",
-        "success_url": "https://dear-diary-psi.vercel.app/?upgraded=true",
-        "cancel_url": "https://dear-diary-psi.vercel.app/?cancelled=true",
+        "success_url": "https://dear-diary-git-main-halliegracehm-5351s-projects.vercel.app/?upgraded=true",
+"cancel_url": "https://dear-diary-git-main-halliegracehm-5351s-projects.vercel.app/?cancelled=true",
       }),
     });
 
